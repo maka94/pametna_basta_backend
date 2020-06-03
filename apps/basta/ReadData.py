59c58233ecedb42bd1d4cd161c510b6935368b90
Marija <pesicmaria94@gmail.com>
2020-06-03 12:27:10 +0200

    ReadData reads senzor data from Arduino

--- apps/basta/ReadData.py
+++ apps/basta/ReadData.py
@@ -1,37 +1,37 @@
 import time, binascii, requests, serial
 
 #serijski port za arduino:
-#ser = serial.Serial('smth')
+ser = serial.Serial("/dev/ttyUSB0", 115200)
 
 #primanje podataka sa arduina:
 
 while True:
-    #response = ser.readline()
-    #pocetak = 'Humidity:'
+    response = ser.readline().decode("UTF-8")
+    vlaznost_zemljista = response.split(';')[0]
+    kisa = response.split(';')[1]
+    temp = response.split(';')[2]
+    vlaznost_vazduha = response.split(';')[3]
+    zalivanje = response.split(';')[4]
+    otvoren_prozor = response.split(';')[5]
+    radi_ventilator = response.split(';')[6]
+    print(response)
 
-    #if response[0:9] == pocetak:
-        #trenutna_vlaznost_zemljista = binascii.b2a_qp(response).split(',')[0]
-        #trenutna_temperatura = binascii.b2a_qp(response).split(',')[1]
-        # dodati/izmeniti senzore
 
-    #vlaznost_zemljista = trenutna_vlaznost_zemljista[10:15]
-    #temperatura = trenutna_temperatura[14:19]
 
-    url = 'http://localhost:8000/basta/read_data'
-    temperatura = 24
-    vlaznost_vazduha = 30
-    vazdusni_pritisak = 20
-    vlaznost_zemljista = 35
+    #url = 'http://localhost:8000/basta/read_data'
 
-    values = {'temp': temperatura, 'vlaznost_vazduha': vlaznost_vazduha, 'vazdusni_pritisak': vazdusni_pritisak,
-              'vlaznost_zemljista': vlaznost_zemljista}
 
-    response = requests.post(url=url, json=values)
-    if response.status_code == 200:
-        print("Podaci uspesno sacuvani u bazi")
-    else:
-        print(response.text)
+    #values = {'vlaznost_zemljista': vlaznost_zemljista, 'kisa':kisa, 'temp': temperatura,
+             # 'vlaznost_vazduha': vlaznost_vazduha, 'zalivanje': zalivanje, 'otvoren_prozor': otvoren_prozor,
+             # 'ventilator': radi_ventilator}
+
+    #response = requests.post(url=url, json=values)
+    #if response.status_code == 200:
+        #print("Podaci uspesno sacuvani u bazi")
+    #else:
+       # print(response.text)
     time.sleep(10)
+
 
 
 
--- apps/basta/relejon.py
+++ apps/basta/relejon.py
@@ -3,4 +3,4 @@ import serial
 komunikacija = serial.Serial("/dev/ttyUSB0", 115200)
 
 sifra = 2
-komunikacija.write(sifra)
+komunikacija.write(str(sifra).encode("UTF-8"))
