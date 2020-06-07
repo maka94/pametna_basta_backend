6f8f6508ec79a6767491e68dc18382b536b6823a
Marija <pesicmaria94@gmail.com>
2020-06-07 21:24:29 +0200

    Change serializers

--- apps/basta/serializers.py
+++ apps/basta/serializers.py
@@ -4,20 +4,24 @@ from apps.basta.models import ArhivskaTabela, DnevnaTabela
 class IstorijaZalivanjaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArhivskaTabela
-        fields = ['datum', 'broj_zalivanja', 'prosecna_temperatura', 'prosecna_vlaznost_vazduha', 'prosecni_vazdusni_pritisak',
-                  'prosecna_vlaznost_zemljista']
+        fields = ['datum', 'broj_zalivanja', 'prosecna_temperatura', 'prosecna_vlaznost_vazduha',
+                  'prosecna_vlaznost_zemljista', 'prosek_kisa']
 
 
 class TrenutniUsloviSerializer(serializers.ModelSerializer):
     class Meta:
         model = DnevnaTabela
-        fields = ['datum', 'temperatura', 'vlaznost_vazduha', 'vazdusni_pritisak', 'vlaznost_zemljista']
+        fields = ['datum', 'temperatura', 'vlaznost_vazduha', 'vlaznost_zemljista', 'kisa', 'zalivanje',
+                  'otvoren_prozor', 'radi_ventilator']
 
 class PodaciSaSenzoraSerializer(serializers.Serializer):
     temp = serializers.DecimalField(max_digits=15, decimal_places=2)
     vlaznost_vazduha = serializers.DecimalField(max_digits=15, decimal_places=2)
-    vazdusni_pritisak = serializers.DecimalField(max_digits=15, decimal_places=2)
     vlaznost_zemljista = serializers.DecimalField(max_digits=14, decimal_places=2)
+    kisa = serializers.DecimalField(max_digits=15, decimal_places=2)
+    zalivanje = serializers.BooleanField(default=False)
+    otvoren_prozor = serializers.BooleanField(default=False)
+    radi_ventilator = serializers.BooleanField(default=False)
 
 class RelaySerializer(serializers.Serializer):
-    ukljuci = serializers.BooleanField(default=False)
+    ukljuci = serializers.IntegerField()
