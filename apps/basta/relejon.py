2f1e1d0228b5fb2781c4e269478abc988c8d7419
Marija <pesicmaria94@gmail.com>
2020-06-07 21:23:49 +0200

    Some lines commented

--- apps/basta/relejon.py
+++ apps/basta/relejon.py
@@ -1,8 +1,8 @@
 import serial, time
 
-komunikacija = serial.Serial("/dev/ttyUSB0", 115200)
+#komunikacija = serial.Serial("/dev/ttyUSB0", 115200)
 
 
 def komanda(sifra):
     time.sleep(5)
-    komunikacija.write(str(sifra).encode("UTF-8"))
+    #komunikacija.write(str(sifra).encode("UTF-8"))
