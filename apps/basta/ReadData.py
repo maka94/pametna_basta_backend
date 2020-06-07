8bf968480398ffca526f11252cf8d5f0b322aeee
Marija <pesicmaria94@gmail.com>
2020-06-07 19:30:27 +0200

    ReadData reads data from senzor

--- apps/basta/ReadData.py
+++ apps/basta/ReadData.py
@@ -1,38 +1,35 @@
-import time, binascii, requests, serial
+import time, requests, serial
 
 #serijski port za arduino:
-ser = serial.Serial("/dev/ttyUSB0", 115200)
+#ser = serial.Serial("/dev/ttyUSB0", 115200)
+
 
 #primanje podataka sa arduina:
 
-while True:
-    response = ser.readline().decode("UTF-8")
-    vlaznost_zemljista = response.split(';')[0]
-    kisa = response.split(';')[1]
-    temp = response.split(';')[2]
-    vlaznost_vazduha = response.split(';')[3]
-    zalivanje = response.split(';')[4]
-    otvoren_prozor = response.split(';')[5]
-    radi_ventilator = response.split(';')[6]
-    print(response)
+#while True:
+    #response = ser.readline().decode("UTF-8")
+    #vlaznost_zemljista = response.split(';')[0]
+    #kisa = response.split(';')[1]
+    #temp = response.split(';')[2]
+    #vlaznost_vazduha = response.split(';')[3]
+    #zalivanje = response.split(';')[4]
+    #otvoren_prozor = response.split(';')[5]
+    #radi_ventilator = response.split(';')[6]
+    #print(response)
 
 
 
-    #url = 'http://localhost:8000/basta/read_data'
+url = 'http://172.20.222.226:8000/basta/read_data'
+values = {"temp": 25, "vlaznost_vazduha": 15, "vazdusni_pritisak":18, "vlaznost_zemljista": 46}
 
 
-    #values = {'vlaznost_zemljista': vlaznost_zemljista, 'kisa':kisa, 'temp': temperatura,
-             # 'vlaznost_vazduha': vlaznost_vazduha, 'zalivanje': zalivanje, 'otvoren_prozor': otvoren_prozor,
-             # 'ventilator': radi_ventilator}
+    #values = {'vlaznost_zemljista': 2, 'kisa':3, 'temp': 4,
+             #'vlaznost_vazduha': 6, 'zalivanje': 1, 'otvoren_prozor': 1,
+             #'ventilator': 1}
 
-    #response = requests.post(url=url, json=values)
-    #if response.status_code == 200:
-        #print("Podaci uspesno sacuvani u bazi")
-    #else:
-       # print(response.text)
-    time.sleep(10)
-
-
-
-
-
+response = requests.post(url=url, json=values)
+if response.status_code == 200:
+    print("Podaci uspesno sacuvani u bazi")
+else:
+    print(response.text)
+    #time.sleep(10)
--- apps/basta/relejoff.py
+++
@@ -1,1 +0,0 @@
-print("relej off")
